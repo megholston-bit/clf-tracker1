f3c6f2e5481162636100193d4710cef399836c0f
megholston-bit <megholston@gmail.com>
2026-08-25 09:31:22 -0700

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import CLFHackathon from './App';
+
+createRoot(document.getElementById('root')).render(<CLFHackathon />);
